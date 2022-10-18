3c0ae20766aac70d52f73b92792c39c7200f66a8
Milton Figueredo Miles <gratno@gmail.com>
2022-10-18 18:43:28 -0500

    Completando errores de validación

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -2,16 +2,41 @@
 
 export function valida(input) {
     const tipoDeInput = input.dataset.tipo;
-    if(validadores[tipoDeInput]) {
+    if (validadores[tipoDeInput]) {
         validadores[tipoDeInput](input);
     }
+    //console.log(input.parentElement)
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid")
+    } else {
+        input.parentElement.classList.add("input-container--invalid");
+    }
 }
-    const validadores = {
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "Este campo no puede estar vacío."
+    },
+    email: {
+        valueMissing: "Este campo no puede estar vacío.",
+        typeMismatch: "El correo no en válido."
+    },
+    password: {
+        valueMissing: "Este campo no puede estar vacío.",
+        patternMismatch: "Mínimo 6 caracteres máximo 12. Debe contener 1 letra minúscula. 1 letra mayúscula. 1 número. No puede contener carácteres especiales."
+    },
+    nacimiento: {
+        valueMissing: "Este campo no puede estar vacío.",
+        customError: "Debes tener al menos 18 años.",
+    }
+}
+
+const validadores = {
     nacimiento: input => validarNacimiento(input),
 };
 
-    /*inputNacimiento.addEventListener("blur", (evento) => {
-    validarNacimiento(evento.target);
+/*inputNacimiento.addEventListener("blur", (evento) => {
+validarNacimiento(evento.target);
 })*/
 
 function validarNacimiento(input) {
@@ -29,6 +54,6 @@ function mayorDeEdad(fecha) {
         fecha.getUTCFullYear() + 18,
         fecha.getUTCMonth(),
         fecha.getUTCDate()
-        );
-    return  diferenciaFechas <= fechaActual;
+    );
+    return diferenciaFechas <= fechaActual;
 }
