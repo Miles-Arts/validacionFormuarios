fc67d81b0e7a443ddfa7bca5721015a083f28283
Milton Figueredo Miles <gratno@gmail.com>
2022-10-18 17:19:15 -0500

    Formulario Contreseña y Fechas

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -1,13 +1,23 @@
-const inputNacimiento = document.querySelector("#birth");
+//const inputNacimiento = document.querySelector("#birth");
 
-inputNacimiento.addEventListener("blur", (evento) => {
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+}
+    const validadores = {
+    nacimiento: input => validarNacimiento(input),
+};
+
+    /*inputNacimiento.addEventListener("blur", (evento) => {
     validarNacimiento(evento.target);
-})
+})*/
 
 function validarNacimiento(input) {
     const fechaCliente = new Date(input.value);
     let mensaje = "";
-    if (mayorDeEdad(fechaCliente)) {
+    if (!mayorDeEdad(fechaCliente)) {
         mensaje = "Debes tener al menos 18 años";
     }
     input.setCustomValidity(mensaje);
@@ -18,6 +28,7 @@ function mayorDeEdad(fecha) {
     const diferenciaFechas = new Date(
         fecha.getUTCFullYear() + 18,
         fecha.getUTCMonth(),
-        fecha.getUTCDate())
-    return  fechaActual <= diferenciaFechas;
+        fecha.getUTCDate()
+        );
+    return  diferenciaFechas <= fechaActual;
 }
