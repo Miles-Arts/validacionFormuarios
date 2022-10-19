b83e7e012fad8e27d8f4d8d6f7d7752559ffff3f
Milton Figueredo Miles <gratno@gmail.com>
2022-10-19 15:03:57 -0500

    Mejorando inputs error

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -7,33 +7,57 @@ export function valida(input) {
     }
     //console.log(input.parentElement)
     if (input.validity.valid) {
-        input.parentElement.classList.remove("input-container--invalid")
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+
     } else {
         input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeError(tipoDeInput, input);
     }
 }
 
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+];
+
 const mensajesDeError = {
     nombre: {
-        valueMissing: "Este campo no puede estar vacío."
+        valueMissing: "El campo nombre no puede estar vacío.",
+        typeMismatch: "Nombre no válido."
     },
     email: {
-        valueMissing: "Este campo no puede estar vacío.",
+        valueMissing: "El campo email no puede estar vacío.",
         typeMismatch: "El correo no en válido."
     },
     password: {
-        valueMissing: "Este campo no puede estar vacío.",
+        valueMissing: "El campo password no puede estar vacío.",
         patternMismatch: "Mínimo 6 caracteres máximo 12. Debe contener 1 letra minúscula. 1 letra mayúscula. 1 número. No puede contener carácteres especiales."
     },
     nacimiento: {
         valueMissing: "Este campo no puede estar vacío.",
-        customError: "Debes tener al menos 18 años.",
+        customError: "Debes tener al menos 18 años o una autorización especial.",
     }
 }
 
 const validadores = {
     nacimiento: input => validarNacimiento(input),
 };
+
+function mostrarMensajeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach(error => {
+        if (input.validity[error]) {
+            console.log(tipoDeInput, error);
+            console.log(input.validity[error]);
+            console.log(mensajesDeError[tipoDeInput][error]);
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    });
+    return mensaje;
+}
 
 /*inputNacimiento.addEventListener("blur", (evento) => {
 validarNacimiento(evento.target);
